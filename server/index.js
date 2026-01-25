8cea4e7ae75269a80595ba70313524f697d48aa8
developerspace-acet <developerspace916@gmail.com>
2026-01-25 13:38:19 +0530

    server initial setup done

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express, { response } from 'express';
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT;
+const url = process.env.ENVIRONMENT == 'development' ? `http://localhost:${port}` : process.env.PRODUCTION_URL; 
+
+app.get("/", (request, response) => {
+    response.status(200).send("<h1>Server Running</h1>");
+})
+
+app.listen(8000, () => console.log(`server running on ${url}`));
